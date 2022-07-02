7fa9fb46212ca1ceebae6026dbad4c3414924ace
Skyz03 <aakibdiscoo@gmail.com>
2022-07-02 14:05:55 +0545

    feat:about section added

---
+++ src/elements/About.svelte
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<section class="about-us">
+  <div class="container">
+    <div class="row about-us__row">
+      <div class="col-lg-5 about-us__img">
+        <img class="img-fluid" src="/images/wedding.jpg" alt="about-us-img" />
+      </div>
+      <div class="col-lg-6 about-us__text">
+        <h3 class="fw-bold about-us__title">About Us</h3>
+        <p class="text-muted  about-us__subtitle">
+          Subha Shringar Decorations provides decoration services of various
+          kinds as required by our valued customers and make the event even more
+          special including balloon decoration, flower decoration and many other
+          celebration items suitable for every events.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  .about-us {
+    padding: 4rem 2rem;
+
+    &__row {
+      align-items: center;
+    }
+
+    &__text {
+      margin-left: 2rem;
+    }
+
+    &__title {
+      font-family: "Calligraffitti", cursive;
+      font-size: 3rem;
+      letter-spacing: 5px;
+    }
+
+    &__subtitle {
+      text-align: justify;
+      letter-spacing: 1px;
+      font-size: 1.1rem;
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,10 @@
   import Carousel from "../elements/Carousel.svelte";
   // @ts-ignore
   import Navigation from "../elements/Navigation.svelte";
+  // @ts-ignore
+  import About from "../elements/About.svelte";
 </script>
 
 <Navigation />
 <Carousel />
+<About />
