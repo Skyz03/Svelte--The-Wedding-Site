d843f7db607b2f3b37e0e8e72c04b5d963d5fa91
Skyz03 <aakibdiscoo@gmail.com>
2022-07-02 11:01:08 +0545

    feat: navbar completed with simple design

--- src/elements/Carousel.svelte
+++ src/elements/Carousel.svelte
@@ -26,27 +26,35 @@
     />
   </div>
   <div class="carousel-inner">
-    <div class="carousel-item active">
+    <div class="carousel-item active  rel">
       <img
         src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VkZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        class="d-block w-100"
+        class="d-block w-100 img-fluid"
         alt="..."
       />
+      <div class="d-flex flex-column rand-title">
+        <h1 class="">Text 2</h1>
+        <button class="custom-btn btn-5"><span>Read More</span></button>
+      </div>
     </div>
-    <div class="carousel-item">
+    <!-- <div class="carousel-item">
       <img
         src="https://images.unsplash.com/photo-1634729108541-516d16ddceec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        class="d-block w-100"
+        class="d-block w-100 img-fluid"
         alt="..."
       />
-    </div>
-    <div class="carousel-item">
+      <div class="d-flex h-100 align-items-center justify-content-center">
+        <h1>Text 2</h1>
+      </div>
+    </div> -->
+    <!-- <div class="carousel-item">
       <img
         src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        class="d-block w-100"
+        class="d-block w-100 img-fluid"
         alt="..."
       />
-    </div>
+      <span>asdsad</span>
+    </div> -->
   </div>
   <button
     class="carousel-control-prev"
@@ -69,4 +77,77 @@
 </div>
 
 <style>
+  .rel {
+    position: relative;
+  }
+  .rand-title {
+    position: absolute;
+    top: 50%;
+    left: 48%;
+  }
+
+  button {
+    margin: 20px;
+  }
+  .custom-btn {
+    width: 130px;
+    height: 40px;
+    color: #fff;
+    border-radius: 5px;
+    padding: 10px 25px;
+    font-family: "Lato", sans-serif;
+    font-weight: 500;
+    background: transparent;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    position: relative;
+    display: inline-block;
+    box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+      7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+    outline: none;
+  }
+
+  /* 5 */
+  .btn-5 {
+    width: 130px;
+    height: 40px;
+    line-height: 42px;
+    padding: 0;
+    border: none;
+    background: rgb(255, 27, 0);
+    background: linear-gradient(
+      0deg,
+      rgba(255, 27, 0, 1) 0%,
+      rgba(251, 75, 2, 1) 100%
+    );
+  }
+  .btn-5:hover {
+    color: #f0094a;
+    background: transparent;
+    box-shadow: none;
+  }
+  .btn-5:before,
+  .btn-5:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 2px;
+    width: 0;
+    background: #f0094a;
+    box-shadow: -1px -1px 5px 0px #fff, 7px 7px 20px 0px #0003,
+      4px 4px 5px 0px #0002;
+    transition: 400ms ease all;
+  }
+  .btn-5:after {
+    right: inherit;
+    top: inherit;
+    left: 0;
+    bottom: 0;
+  }
+  .btn-5:hover:before,
+  .btn-5:hover:after {
+    width: 100%;
+    transition: 800ms ease all;
+  }
 </style>
--- src/elements/Navigation.svelte
+++ src/elements/Navigation.svelte
@@ -1,66 +1,77 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light navigation fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img class="navigation__img" src="/images/nav-logo.png" alt="logo-img" />
-    </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div
-      class="collapse navbar-collapse justify-content-evenly"
-      id="navbarSupportedContent"
-    >
-      <ul class="navbar-nav navigation__menu">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
+<script></script>
 
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Pages
-          </a>
-          <ul
-            class="dropdown-menu navigation__dropdown"
-            aria-labelledby="navbarDropdown"
-          >
-            <li><a class="dropdown-item" href="#">Wedding</a></li>
-            <li><a class="dropdown-item" href="#">Birthday</a></li>
-            <li><a class="dropdown-item" href="#">Mehdi</a></li>
-            <li><a class="dropdown-item" href="#">Mehdi</a></li>
-            <li><a class="dropdown-item" href="#">Mehdi</a></li>
-            <li><a class="dropdown-item" href="#">Mehdi</a></li>
-            <li><a class="dropdown-item" href="#">Mehdi</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
+<div>
+  <nav
+    class="navbar navbar-expand-lg navbar-light bg-light navigation fixed-top"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img
+          class="navigation__img"
+          src="/images/nav-logo.png"
+          alt="logo-img"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div
+        class="collapse navbar-collapse justify-content-evenly"
+        id="navbarSupportedContent"
+      >
+        <ul class="navbar-nav navigation__menu">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Services
+            </a>
+            <ul
+              class="dropdown-menu navigation__dropdown"
+              aria-labelledby="navbarDropdown"
+            >
+              <li><a class="dropdown-item" href="#">Wedding</a></li>
+              <li><a class="dropdown-item" href="#">Baby Shower</a></li>
+              <li><a class="dropdown-item" href="#">Birthday</a></li>
+              <li><a class="dropdown-item" href="#">Bartamandha</a></li>
+              <li><a class="dropdown-item" href="#">Anniversary</a></li>
+              <li><a class="dropdown-item" href="#">Mehdi</a></li>
+              <li><a class="dropdown-item" href="#">Pasni</a></li>
+              <li>
+                <a class="dropdown-item" href="#">Surprise Party Decorations</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</div>
 
 <style lang="scss">
   .navigation {
-    // background: #fffbd5;
-    // background: -webkit-linear-gradient(to left, #b20a2c, #fffbd5);
-    // background: linear-gradient(to left, #b20a2c, #fffbd5);
     background: transparent;
+    padding: 1rem;
 
     &__img {
       width: 100%;
@@ -69,9 +80,17 @@
     }
 
     &__menu {
+      gap: 3rem;
+
+      @media (max-width: 991.98px) {
+        gap: 1rem;
+      }
+
       li {
         a {
-          color: #fff;
+          color: #ce3c3d;
+          font-weight: 600;
+          text-transform: uppercase;
         }
       }
     }
