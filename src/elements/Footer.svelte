9032f7644dd871c076d1044fdb5e42f686f904d9
Skyz03 <aakibdiscoo@gmail.com>
2022-07-02 20:59:19 +0545

    Feat: footer added

--- src/elements/Footer.svelte
+++ src/elements/Footer.svelte
@@ -28,7 +28,7 @@
             <a href="#" class="nav-link p-0 text-muted">Birthday</a>
           </li>
           <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Bartamandha</a>
+            <a href="#" class="nav-link p-0 text-muted">Photography</a>
           </li>
         </ul>
       </div>
@@ -43,7 +43,7 @@
             <a href="#" class="nav-link p-0 text-muted">Mehdi</a>
           </li>
           <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Pasni</a>
+            <a href="#" class="nav-link p-0 text-muted">Flower Decoration</a>
           </li>
           <li class="nav-item mb-2">
             <a href="#" class="nav-link p-0 text-muted">Surprise Party</a>
@@ -51,18 +51,22 @@
         </ul>
       </div>
 
-      <div class="col-xl-3">
+      <div class="col-xl-3 footer__contact">
         <h5>Contact</h5>
         <ul class="nav flex-column footer__lists">
           <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">+977 9812576842</a>
+            <img src="/images/icons/call-icon.png" alt="" />
+            <a href="#" class="nav-link p-0 text-muted">+977 98188768757</a>
           </li>
           <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">info@contact.com</a>
+            <img src="/images/icons/email-icon.png" alt="" />
+            <a href="#" class="nav-link p-0 text-muted">info@subashringar.com</a
+            >
           </li>
           <li class="nav-item mb-2">
+            <img src="/images/icons/map-icon.png" alt="" />
             <a href="#" class="nav-link p-0 text-muted"
-              >Ason, Kathmandu, Nepal</a
+              >Banshidhar Marga, Chandol Chowk, Kathmandu</a
             >
           </li>
         </ul>
@@ -73,12 +77,18 @@
       <p>&copy; 2022 Company, Inc. All rights reserved.</p>
       <ul class="list-unstyled d-flex footer__lists">
         <li class="ms-3">
-          <a class="link-dark" href="#">
+          <a
+            class="link-dark"
+            href="https://www.facebook.com/subhashringardecorations/"
+          >
             <img src="/images/fb.png" alt="" />
           </a>
         </li>
         <li class="ms-3">
-          <a class="link-dark" href="#">
+          <a
+            class="link-dark"
+            href="https://www.instagram.com/subhashringardecorations/"
+          >
             <img src="/images/insta.png" alt="" />
           </a>
         </li>
@@ -93,6 +103,15 @@
     color: #fff;
     padding: 3rem 3rem 0;
 
+    h5 {
+      font-weight: 600;
+      font-size: 1.2rem;
+      letter-spacing: 0.05em;
+      padding-bottom: 0.5rem;
+      width: 50%;
+      border-bottom: 1px solid #ffff;
+    }
+
     &__img {
       width: 100%;
       height: 100%;
@@ -102,8 +121,28 @@
     &__lists {
       padding: 1rem 0;
       li {
+        letter-spacing: 0.07em;
+        transition: all 0.5s ease-out;
+
+        &:hover {
+          opacity: 0.7;
+          transform: translateY(-0.3rem);
+        }
         a {
           color: #fff !important;
+        }
+      }
+    }
+
+    &__contact {
+      li {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        img {
+          max-width: 25px;
+          width: 100%;
         }
       }
     }
