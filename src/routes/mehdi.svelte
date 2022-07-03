941aaf2c43f0e8d96ac753a1e77f3828f7abc27e
Skyz03 <aakibdiscoo@gmail.com>
2022-07-03 14:50:51 +0545

    baby shower & mehdi page added

---
+++ src/routes/mehdi.svelte
@@ -0,0 +1,16 @@
+<script>
+  // @ts-ignore
+  import Carousel from "../elements/Carousel.svelte";
+  // @ts-ignore
+  import Navigation from "../elements/Navigation.svelte";
+  // @ts-ignore
+  import Footer from "../elements/Footer.svelte";
+  import Heading from "../elements/Heading.svelte";
+  import App from "../Mehdi/Mehdi.svelte";
+</script>
+
+<Navigation />
+
+<Heading title={"Mehdi Service"} />
+<App />
+<Footer />
